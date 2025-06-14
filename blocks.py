6fe244af95da0116bade509fa29c4a66e5743936
Ishushan02 <ishananand2002@gmail.com>
2025-06-14 15:55:50 -0700

    Transformer Attention Block

--- blocks.py
+++ blocks.py
@@ -19,5 +19,29 @@ class FeedForward(nn.Module):
         return x
     
 
+# x = torch.randn(10, 64, 64)
+# ffModel = FeedForward(64, 100, 10)
+
+# out = ffModel(x)
+# print(out.shape)
 
 
+class TransformerEncoder(nn.Module):
+    def __init__(self, nEmbed, numHeads, mlpOut, drop_rate = 0.2):
+        super().__init__()
+        self.nEmbed = nEmbed
+        self.numHeads = numHeads
+
+        self.norm1 = nn.LayerNorm(nEmbed)
+        self.attention = nn.MultiheadAttention(embed_dim=nEmbed, num_heads=numHeads, dropout=drop_rate, batch_first=True)
+        self.mlp = FeedForward(nEmbed, 2 * nEmbed, nEmbed, drop_rate)
+
+    def forward(self, x):
+        x = x + self.attention(self.norm1(x), self.norm1(x), self.norm1(x))[0]
+        x = x + self.mlp(self.norm1(x))
+        return x
+    
+# x = torch.randn(10, 64, 64)  
+# att = TransformerEncoder(64, 8, 10)
+# out = att(x)
+# print(out.shape)  # should be [10, 64, 10] if mlpOut = 10
